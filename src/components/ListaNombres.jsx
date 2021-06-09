a053e807bca5ea89006676acfbab1e7d2b701c59
Marcelo Bettini <bettinimarcelo@gmail.com>
2021-06-08 21:33:12 -0300

    crea objeto con id único y nombre

--- src/components/ListaNombres.jsx
+++ src/components/ListaNombres.jsx
@@ -1,6 +1,18 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from "react"
+import uniqid from 'uniqid'
 
 const ListaNombres = () => {
+  const [nombre, setNombre] = useState('')
+  const [listaNombres, setListaNombres] = useState([])
+  function addNombre(e) {
+    e.preventDefault()
+    const nombreUnico = {
+      id: uniqid(),
+      nombre:nombre
+
+    }
+    setListaNombres([...listaNombres, nombreUnico]) //spread operator instead of concat method
+  }
   return (
     <Fragment>
       <h3 className="display-6">CRUD Basic App</h3>
@@ -10,14 +22,15 @@ const ListaNombres = () => {
         </div>
         <div className="col">
           <h4>Formulario para añadir nombres</h4>
-          <form className="form-group">
+          <form onSubmit={(e) => addNombre(e)} addclassName="form-group">
             <input
+              onChange={(e) => {setNombre(e.target.value)}}
               className="form-control mt-2"
               type="text"
               placeholder="Introduce el nombre"
             />
             <div className="d-grid">
-              <input
+              <input                
                 className="btn btn-primary mt-2"
                 type="submit"
                 value="Agregar"
