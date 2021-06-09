8b9044d79159163eea7c94855420d22742270626
Marcelo Bettini <marcelobettini@yahoo.com.ar>
2021-06-09 12:34:51 -0300

    add, edit and delete working OK

--- src/components/ListaNombres.jsx
+++ src/components/ListaNombres.jsx
@@ -1,21 +1,54 @@
-import React, { Fragment, useState } from "react"
-import uniqid from 'uniqid' 
+import React, { Fragment, useState } from "react";
+import uniqid from "uniqid";
 
 const ListaNombres = () => {
-  const [nombre, setNombre] = useState('')
-  const [listaNombres, setListaNombres] = useState([])
+  const [nombre, setNombre] = useState("");
+  const [listaNombres, setListaNombres] = useState([]);
+  const [edicion, setEdicion] = useState(false);
+  const [id, setId] = useState("");
+  const [error, setError] = useState(null)
+
   function addNombre(e) {
-    e.preventDefault()
+    e.preventDefault();
     const nombreUnico = {
       id: uniqid(),
-      nombre:nombre
+      nombre: nombre,
+    };
+    if (!nombre.trim()) {
+      setError("nombre debe tener algo, man");
+    } else {
+      setListaNombres([...listaNombres, nombreUnico]); //spread operator instead of concat method
+      setError(null)
+      setNombre("");
     }
-    setListaNombres([...listaNombres, nombreUnico]) //spread operator instead of concat method
-    setNombre('')    
+    
+    
   }
+
   function deleteNombre(id) {
-    const arrMenosUno = listaNombres.filter( item => item.id !== id)
-    setListaNombres(arrMenosUno)
+    const arrMenosUno = listaNombres.filter((item) => item.id !== id);
+    setListaNombres(arrMenosUno);
+  }
+
+  function setForEdit(item) {
+    setEdicion(true);
+    setNombre(item.nombre);
+    setId(item.id);
+  }
+
+  function editNombre(e) {
+    e.preventDefault();
+    if (!nombre.trim()) {
+      setError("nombre debe tener algo, man");
+    } else {
+      const arrEdited = listaNombres.map((item) =>
+        item.id === id ? { id: id, nombre: nombre } : item
+      );
+      setListaNombres(arrEdited);
+      setEdicion(false);
+      setNombre("");  
+      setError(null)    
+    }
   }
   return (
     <Fragment>
@@ -23,32 +56,48 @@ const ListaNombres = () => {
       <div className="row">
         <div className="col">
           <h4>Listado de nombres</h4>
-          <ul className="list-group">            
-              {
-              listaNombres.map(item => 
-                <li key={item.id} className="list-group-item">{item.nombre} 
-                <button onClick={ () => deleteNombre(item.id)} className="btn btn-sm btn-danger float-end">X</button>
-                </li>)
-
-              }
+          <ul className="list-group">
+            {listaNombres.map((item) => (
+              <li key={item.id} className="list-group-item">
+                {item.nombre}
+                <button
+                  onClick={() => deleteNombre(item.id)}
+                  className="btn btn-sm btn-danger float-end"
+                >
+                  X
+                </button>
+                <button
+                  onClick={() => setForEdit(item)}
+                  className="btn btn-sm btn-success float-end"
+                >
+                  ||
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="col">
           <h4>Formulario para añadir nombres</h4>
-          <form onSubmit={(e) => addNombre(e)} className="form-group">
+          <form
+            onSubmit={edicion ? editNombre : addNombre}
+            className="form-group"
+          >
             <input
               onChange={(e) => setNombre(e.target.value)}
-              className="form-control mt-2"         
-              id="input"     
+              className="form-control mt-2"
+              id="input"
               type="text"
               placeholder="Introduce el nombre"
               value={nombre}
             />
             <div className="d-grid">
-              <input                
+              {
+                error != null ? (<div className="alert alert-danger">{error}</div>) : (<div></div>)   
+              }
+              <input
                 className="btn btn-primary mt-2"
                 type="submit"
-                value="Agregar"
+                value={edicion ? "Modificar" : "Agregar"}
               />
             </div>
           </form>
